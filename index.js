5267703de4d4fbb30e33fe873e666c4af071c123
cjfaquino <66150833+cjfaquino@users.noreply.github.com>
2022-10-30 22:51:57 +0000

    Deploying to gh-pages from @ cjfaquino/testing-github-secrets@c73945c51a5b93e2f28efa53f9c9d5de84eacc46 🚀

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";document.querySelector("h1").textContent=`${process.env.TEST}`,console.log(process.env.TEST)})();
+(()=>{"use strict";document.querySelector("h1").textContent="".concat(process.env.TEST),console.log(process.env.TEST);var e=[[1,2],3].flat();console.log(e)})();
