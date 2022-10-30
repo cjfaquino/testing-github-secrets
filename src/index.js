c73945c51a5b93e2f28efa53f9c9d5de84eacc46
Christian Aquino <cjfaquino@gmail.com>
2022-10-30 15:39:55 -0700

    add babel-loader, css-minimizer

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ const h1 = document.querySelector('h1');
 h1.textContent = `${process.env.TEST}`;
 
 console.log(process.env.TEST);
+const test = [[1, 2], 3].flat();
+console.log(test);
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const { merge } = require('webpack-merge');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const common = require('./webpack.config');
 
 module.exports = merge(common, {
@@ -12,6 +13,24 @@ module.exports = merge(common, {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      `...`,
+      new CssMinimizerPlugin(),
     ],
   },
 });
